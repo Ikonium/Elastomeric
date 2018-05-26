64e3223280ab627b93724b49e01b7f50d075f599
Christopher Cohen <candyforchris@gmail.com>
2018-05-25 17:47:09 -0700

    Initial Commit

---
+++ Elastomeric/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Elastomeric
+//
+//  Created by Christopher Cohen on 5/25/18.
+//  Copyright © 2018 Ikonium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
